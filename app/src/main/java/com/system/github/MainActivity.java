5ccfc6787b07b9afe198967245a891db1a82ffec
XuLong <1119641287@qq.com>
2016-11-23 21:14:26 +0800

    修改ImageView

--- app/src/main/java/com/system/github/MainActivity.java
+++ app/src/main/java/com/system/github/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = new Button(this);
         TextView view = new TextView(this);
+        ImageView imageView = new ImageView(this);
     }
 
     @Override
