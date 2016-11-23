b493274c69ef5ece527de61e44d4d2ede42c73a8
XuLong <1119641287@qq.com>
2016-11-23 21:03:48 +0800

    我修改了一个TEXTvIEW

--- app/src/main/java/com/system/github/MainActivity.java
+++ app/src/main/java/com/system/github/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = new Button(this);
+        TextView view = new TextView(this);
     }
 
     @Override
